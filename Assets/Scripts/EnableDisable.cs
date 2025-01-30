fbd8c597fb7c830b29233183490b99b08ca46bcf
Zhanming Liu <liuzhanm@shernet.sheridancollege.ca>
2025-01-30 10:50:06 -0500

    Audio codes saved

--- Assets/Scripts/EnableDisable.cs
+++ Assets/Scripts/EnableDisable.cs
@@ -7,11 +7,13 @@ public class EnableDisable : MonoBehaviour
     public GameObject go;
     public SpriteRenderer sr;
     public EnableDisable script;
+    public AudioSource audioSource;
+    public AudioClip clip;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -20,14 +22,24 @@ public class EnableDisable : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             sr.enabled = false;
-            //script.enabled = false;
+            script.enabled = false;
             go.SetActive(false);
         }
         if (Input.GetKey(KeyCode.Space))
         {
             sr.enabled = true;
-            //script.enabled = true;
+            script.enabled = true;
             go.SetActive(true);
+        }
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                {
+                    if (audioSource.isPlaying == false)
+                        //audioSource.Play();
+                        audioSource.PlayOneShot(clip);
+                }
+            }
         }
     }
 }
