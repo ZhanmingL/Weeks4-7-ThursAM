6b2bff27a61d1ce310696a145aeb6bdabf3ae93e
Zhanming Liu <liuzhanm@shernet.sheridancollege.ca>
2025-02-14 14:54:31 -0500

    I fixed this issue! The issue is about using Lerp and Prefabs, then next step, I plan to make enemy instantiate and damages.

--- Assets/Explorable Toy/ExplorableToy Scripts/Bullet.cs
+++ Assets/Explorable Toy/ExplorableToy Scripts/Bullet.cs
@@ -7,12 +7,15 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [Range(0, 1)]
-    public float t = 0; //Moving time range.
+    public float t = 0f; //Moving time range.
+    public float speed = 5f; //Bullet's speed.
 
-    public float speed = 5f;
+    //Initially, I wanted to use public Transform to get positions, but they are not able to be selected in Prefab, so I edited it from Transform to Vector, yeah now is vector2, it works.
+    public Vector2 playerSide; //Shooting position (Player's position).
+    public Vector2 enemySide; //Enymy's position, check arrival position.
 
-    public Transform playerSide; //Shooting position (Player's position).
-    public Transform enemySide; //Enymy's position, check arrival.
+    bool canShoot = false; //This bool statement is super important, I had an issue which is about if I click mouse fastly, many bullets' t values can not be increased,
+                           //because I infered that there is only one t that controls all the prefabs, so now I make a bool, and make all prefabs individually, they don't affect each other anymore!
 
     void Start()
     {
@@ -22,13 +25,24 @@ public class Bullet : MonoBehaviour
 
     void Update()
     {
-            transform.position = Vector2.Lerp(playerSide.position, enemySide.position, t); //Use Lerp to move bullet from player to enemy.
+        if (canShoot)
+        {
+            Destroy(gameObject, 1);
 
-            t += Time.deltaTime * speed; //add t value from 0 to 1.
-
-            if (t == 1)
+            if (t < 1)
+            {
+                t += Time.deltaTime * speed; //add t value from 0 to 1.
+                transform.position = Vector2.Lerp(playerSide, enemySide, t); //Use Lerp to move bullet from player to enemy.
+            }
+            else
             {
                 Destroy(gameObject); //When getting 1, also means bullet get enemy's position, destroy bullet.
             }
+        }
+    }
+
+    public void shoot()
+    {
+        canShoot = true; //Be controlled in player's script, let it become true and do shooting.
     }
 }
--- Assets/Explorable Toy/ExplorableToy Scripts/PlayerMove.cs
+++ Assets/Explorable Toy/ExplorableToy Scripts/PlayerMove.cs
@@ -8,6 +8,11 @@ public class PlayerMove : MonoBehaviour
 
     public GameObject bulletPrefab; //I want bullet generated from original prefab.
 
+    bool mouseDown = false;
+
+    public Transform enemySide; //Enemy's position, I will get it in Unity's inspector.
+
+    Bullet myBullet; //Get a bullet reference.
     void Start()
     {
 
@@ -15,13 +20,27 @@ public class PlayerMove : MonoBehaviour
 
     void Update()
     {
+        //Player's movement.
         Vector2 pos = transform.position;
         pos.x += Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime; //Input of "A""D" and two arrow keys control movement of player (horizontally).
         transform.position = pos;
 
+        //Prefab executing.
         if (Input.GetMouseButtonDown(0))
         {
             GameObject newBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity); //When click mouseButton, generate a new bullet from Prefab.
+            myBullet = newBullet.GetComponent<Bullet>();
+
+            //These codes are giving values when each new prefab(bullet) is generated, because I cannot directly give value in Prefab's Inspector.
+            Bullet wantBulletScript = newBullet.GetComponent<Bullet>(); //I want two Vector2 values from Bullet class, that are playerside and enemyside, then I can give them value.
+            wantBulletScript.playerSide = transform.position; //Give generated bullet the player's position(generated position as well).
+            wantBulletScript.enemySide = enemySide.position; //Give enemy's position.
+
+            mouseDown = true; //The condition of letting bullet's code run.
+        }
+        if (mouseDown == true && myBullet != null) //I make sure that Bullt is exist, then I run it's code.
+        {
+            myBullet.shoot(); //Run bullet moving function, then it is shot.
         }
     }
 }
