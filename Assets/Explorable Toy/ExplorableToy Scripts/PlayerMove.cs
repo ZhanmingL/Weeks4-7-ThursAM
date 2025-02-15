f1f2b1f8f98bcff06f56798e082818f1ab4556b3
Zhanming Liu <liuzhanm@shernet.sheridancollege.ca>
2025-02-15 11:22:43 -0500

    I made a position limitation based on Mathf, and I also made a timer. Next step, I will make enemy Prefab.

--- Assets/Explorable Toy/ExplorableToy Scripts/PlayerMove.cs
+++ Assets/Explorable Toy/ExplorableToy Scripts/PlayerMove.cs
@@ -13,18 +13,49 @@ public class PlayerMove : MonoBehaviour
     public Transform enemySide; //Enemy's position, I will get it in Unity's inspector.
 
     Bullet myBullet; //Get a bullet reference.
+
+    Vector2 minBound, maxBound; //screen bound's minimum values and maximum values.
+
     void Start()
     {
-
+        GetBound(); //In the first frame, I directly count and get the Clamp value.
     }
 
     void Update()
     {
+        MovePlayer(); //Move player.
+
+        ClampPosition(); //Don't get out of screen.
+
+        GenerateBullet(); //Prefab creating when click mouse.
+    }
+
+    private void MovePlayer() //my function of player moving, keys input.
+    {
         //Player's movement.
         Vector2 pos = transform.position;
         pos.x += Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime; //Input of "A""D" and two arrow keys control movement of player (horizontally).
+        pos.y += Input.GetAxisRaw("Vertical") * speed * Time.deltaTime; //Input of "W""S" and two arrow keys control movement.
         transform.position = pos;
+    }
 
+    private void ClampPosition() //It's a function that controls player within the screen.
+    { //this Mathf is important because I don't want my player moves out of screen.
+        Vector2 clampP = transform.position; //Player's position is being restricted by Clamp.
+        clampP.x = Mathf.Clamp(clampP.x, minBound.x,maxBound.x); //x position value's restriction.
+        clampP.y = Mathf.Clamp(clampP.y, minBound.y, maxBound.y); //y position value's restriction.
+        transform.position = clampP;
+    }
+    private void GetBound()
+    {
+        Vector2 min = Camera.main.ScreenToWorldPoint(new Vector3(0, 0)); //I state where the minimum bound's location is.
+        Vector2 max = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height)); //I also state maximum bound's location. Now, these helpful vectors stop player's position within the screen.
+        minBound = new Vector2(min.x, min.y); //Now give these vectoe values back to main one which gonna be used my me.
+        maxBound = new Vector2(max.x, max.y);
+    }
+
+    private void GenerateBullet()
+    {
         //Prefab executing.
         if (Input.GetMouseButtonDown(0))
         {
---
+++ Assets/Explorable Toy/ExplorableToy Scripts/ToyTimer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToyTimer : MonoBehaviour
+{
+    public Slider timer; //use slider to make my game's timer.
+
+    public float maxTime = 100f; //maximum value of my slider is 100.
+
+    void Start()
+    {
+        timer.value = maxTime; //initialize timer value to the maximum.
+    }
+
+
+    void Update()
+    {
+        timer.value -= Time.deltaTime; //timer's value is decreasing everyframe.
+    }
+}
